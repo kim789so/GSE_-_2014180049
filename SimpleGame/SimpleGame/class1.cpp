ac09cd5ec6d74f47759015c2ae7024489fb34cc7
kim sang soo <kim789so@naver.com>
2017-09-27 18:00:56 +0900

    SimpleGame

---
+++ SimpleGame/SimpleGame/class1.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "class1.h"
+
+void CCobject::init(float x, float y, float z, float size, float r, float g, float b, float a)
+{
+	m_x = x;
+	m_y = y;
+	m_z = z;
+	m_size = size;
+	m_r = r;
+	m_g = g;
+	m_b = b;
+	m_a = a;
+}
